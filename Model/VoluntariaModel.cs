39d42717c970a978b1118c60b97b6b5c18c72478
Vanessa Abe <vanessa.ariel24@gmail.com>
2022-05-31 17:30:59 -0300

    wip: voluntaria model and form

--- Model/VoluntariaModel.cs
+++ Model/VoluntariaModel.cs
@@ -7,8 +7,9 @@ namespace DonnaGabriela.Model
     {
 
         DatabaseUtils databaseUtils = new DatabaseUtils();
-        public object getVoluntariaById(String id)
+        public Voluntaria getVoluntariaById(String id)
         {
+            Voluntaria voluntaria = new Voluntaria();
             String query = "SELECT * FROM Voluntario WHERE ID_Voluntario = " + id;
 
             databaseUtils.openConnection();
@@ -17,9 +18,33 @@ namespace DonnaGabriela.Model
             if (reader.HasRows)
             {
                 reader.Read();
-                Console.WriteLine(reader["ID_Voluntario"].ToString());
+                voluntaria.ID_Voluntario = int.Parse(reader["ID_Voluntario"].ToString());
+                voluntaria.ID_Servico = reader["ID_Servico"].ToString();
+                voluntaria.Nome_Servico = reader["Nome_Servico"].ToString();
+                voluntaria.Nome_Voluntario = reader["Nome_Voluntario"].ToString();
+                voluntaria.Telefone_Voluntario = reader["Telefone_Voluntario"].ToString();
+                voluntaria.Email_Voluntario = reader["Email_Voluntario"].ToString();
+                voluntaria.Cep_Voluntario = reader["Cep_Voluntario"].ToString();
+                voluntaria.Endereco_Voluntario = reader["Endereco_Voluntario"].ToString();
+                voluntaria.Numero_Voluntario = reader["Numero_Voluntario"].ToString();
+                voluntaria.Bairro_Voluntario = reader["Bairro_Voluntario"].ToString();
+                voluntaria.Cidade_Voluntario = reader["Cidade_Voluntario"].ToString();
+                voluntaria.Complemento_Voluntario = reader["Complemento_Voluntario"].ToString();
+                voluntaria.Data_Nasci_User = reader["Data_Nasci_User"].ToString();
+                voluntaria.Senha_Voluntario = reader["Senha_Voluntario"].ToString();
+                voluntaria.Sexo_Voluntario = reader["Sexo_Voluntario"].ToString();
+                voluntaria.Data_Cadastro = reader["Data_Cadastro"].ToString();
+                voluntaria.Data_Desligamento = reader["Data_Desligamento"].ToString();
+                voluntaria.Data_Admissao = reader["Data_Admissao"].ToString();
+                voluntaria.Status_Conta = reader["Status_Conta"].ToString();
             }
-            return reader.HasRows;
+            return voluntaria;
+        }
+
+        public Boolean editVoluntaria(Voluntaria voluntaria)
+        {
+            String query = "UPDATE Voluntaria SET ... WHERE ID_Voluntario = " + voluntaria.ID_Voluntario;
+            return false;
         }
     }
 }
